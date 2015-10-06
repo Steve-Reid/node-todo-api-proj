0416a50fdc8a200d4ec16d6d4a7d3b8b5924c306
Steve-Reid <webworksouth@gmail.com>
2015-10-06 23:56:03 +0100

    Added /todos and todos/:id

--- server.js
+++ server.js
@@ -2,8 +2,45 @@ var express = require('express'),
 	app = express(),
 	PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Video tango class',
+	completed: false
+}, {
+	id: 2,
+	description: 'Edit tango videos',
+	completed: false
+}, {
+	id: 3,
+	description: 'Transfer Phil\'s domain name',
+	completed: true
+}];
+
 	app.get('/', function (req, res) {
 		res.send('Todo API Root');
+	});
+
+	// GET /todos
+	app.get('/todos', function (req, res) {
+		res.json(todos);
+	});
+
+	// GET /todos/:id
+	app.get('/todos/:id', function (req, res) {
+		var todoId = parseInt(req.params.id, 10),
+			matchedTodo;
+
+		todos.forEach(function (todo) {
+			if (todoId === todo.id) {
+				matchedTodo = todo;
+			}
+		});
+
+		if (matchedTodo) {
+			res.json(matchedTodo);
+		} else {
+			res.status(404).send();
+		}
 	});
 
 	app.listen(PORT, function () {
