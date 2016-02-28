ae2c4b98e4798beed1adbc4ad20debe0d3c15aef
Steve-Reid <steve-reid@sky.com>
2016-02-28 00:12:34 +0000

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -36,14 +36,17 @@ app.get('/todos', function (req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id, 10),
-		matchedTodo = _.findWhere(todos, {id: todoId});
+	var todoId = parseInt(req.params.id, 10);
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 });
 
 
@@ -56,25 +59,6 @@ app.post('/todos', function (req, res) {
 	}, function (e) {
 		res.status(400).toJSON(e);
 	});
-
-	// call create on db.todo
-	//	respond with 200 and todo
-	//	res.status(400).json(e)
-
-
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-	
-	// body.id = todoNextId;
-	// todoNextId++;
-
-	// body.description = body.description.trim();
-
-	// todos.push(body);
-
-	// res.json(body);
 });
 
 // DELETE /todos/:id
